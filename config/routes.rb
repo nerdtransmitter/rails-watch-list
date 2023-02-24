f0148eaefef6b1a1ee1d19045dd8ede2ba0381ba
Alessia Moison <adm2217@columbia.edu>
2023-02-24 15:36:30 +0100

    added dotenv gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "lists#index"
-  resources :lists , except: :index do
+  resources :lists, except: :index do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: [:destroy]
